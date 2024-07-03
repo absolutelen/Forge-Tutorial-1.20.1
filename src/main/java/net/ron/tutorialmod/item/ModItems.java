1599675da33fc7006dcbd9fffd65c25721e9420e
len <ron@pmt.co.il>
2024-07-03 22:42:23 +0300

    0.4v

--- src/main/java/net/ron/tutorialmod/item/ModItems.java
+++ src/main/java/net/ron/tutorialmod/item/ModItems.java
@@ -15,6 +15,8 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
     public static  final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
             () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ACORN = ITEMS.register("acorn",
+            () -> new Item(new Item.Properties()));
 
 
     public static void register(IEventBus eventBus) {
