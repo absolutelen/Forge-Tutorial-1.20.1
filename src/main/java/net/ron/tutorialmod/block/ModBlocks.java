5a9feae4be7b2b00f2a804cac5dd27f976db9c21
len <ron@pmt.co.il>
2024-07-05 14:28:00 +0300

    0.7v

--- src/main/java/net/ron/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/ron/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,5 @@
 package net.ron.tutorialmod.block;
 
-import com.mojang.blaze3d.shaders.Uniform;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -14,6 +13,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.ron.tutorialmod.TutorialMod;
+import net.ron.tutorialmod.block.custom.SoundBlock;
 import net.ron.tutorialmod.item.ModItems;
 
 import java.util.function.Supplier;
@@ -40,6 +40,9 @@ public class ModBlocks
     public static final RegistryObject<Block> END_STONE_SAPPHIRE_ORE = registerBlock("end_stone_sapphire_ore",
             () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.END_STONE)
                     .strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+
+    public static final RegistryObject<Block> SOUND_BLOCK = registerBlock("sound_block",
+            () -> new SoundBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
